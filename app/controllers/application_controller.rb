9c0237c52934ad0981abb562f70f15ab0c01e8d8
桑原龍樹 <tatsuki.kuwahara@vitalize.co.jp>
2021-07-15 16:00:50 +0900

    Profile編集画面とその後の画面遷移ロジック追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
     if current_user
@@ -14,11 +12,5 @@ class ApplicationController < ActionController::Base
 
   def current_user?
     current_user ? true : false
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(job_id pref_id name name_kana nickname phone birthday gender work_place self_introduction))
   end
 end
