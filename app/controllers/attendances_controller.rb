319691c241e97260af5cb6d2ecae5cae4cb01802
桑原龍樹 <tatsuki.kuwahara@vitalize.co.jp>
2021-07-27 16:07:21 +0900

    indexとshowを分ける

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,15 +1,15 @@
 class AttendancesController < ApplicationController
   def index
     @attendance = current_admin.attendances.today_attendance_scope.first
+  end
+
+  def show
     @attendances = []
     days = ["日", "月", "火", "水", "木", "金", "土"]
     @attendances = current_admin.attendances.week_attendances_scope.map do |attendance|
       working_day = attendance.start_time.strftime('%Y/%m/%d') + "(#{days[attendance.start_time.to_date.wday]})"
       [working_day, attendance.working_time]
     end
-  end
-
-  def show
   end
 
   def new
