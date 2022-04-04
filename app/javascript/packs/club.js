95215c2ad62f446d452e3c1a9b02bfb5ff98352c
Yuuki Tsuji <yuuki.tsuji@banana-877.com>
2022-04-04 18:09:00 +0900

    20章　js動かないバグ修正

--- app/javascript/packs/club.js
+++ app/javascript/packs/club.js
@@ -1,19 +1,21 @@
-$(function () {
-  $('.js-text_field').on('keyup', function () {
-    var name = $.trim($(this).val());
-    $.ajax({
-      type: 'GET', // リクエストのタイプ
-      url: '/clubs/search', // リクエストを送信するURL
-      data:  { name: name }, // サーバーに送信するデータ
-      dataType: 'json' // サーバーから返却される型
-    }).done(function (data) {
-      $('.js-clubs li').remove();
-      // 以下のコードを追加する
-      $(data).each(function(i,club) {
-        $('.js-clubs').append(
-          `<li class="club"><a href="/clubs/${club.id}">${club.name}</a></li>`
-        );
-      });
-    })
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    $('.js-text_field').on('keyup', function () {
+      var name = $.trim($(this).val());
+      $.ajax({
+        type: 'GET', // リクエストのタイプ
+        url: '/clubs/search', // リクエストを送信するURL
+        data:  { name: name }, // サーバーに送信するデータ
+        dataType: 'json' // サーバーから返却される型
+      }).done(function (data) {
+        $('.js-clubs li').remove();
+        // 以下のコードを追加する
+        $(data).each(function(i,club) {
+          $('.js-clubs').append(
+            `<li class="club"><a href="/clubs/${club.id}">${club.name}</a></li>`
+          );
+        });
+      })
+    });
   });
 });
--- app/javascript/packs/event.js
+++ app/javascript/packs/event.js
@@ -1,70 +1,73 @@
-$(function () {
-  $('.js-text_field_title').on('keyup', function () {
-    var title = $.trim($(this).val());
-    $.ajax({
-      type: 'GET', // リクエストのタイプ
-      url: '/events/search', // リクエストを送信するURL
-      data:  { title: title}, // サーバーに送信するデータ
-      dataType: 'json' // サーバーから返却される型
-    }).done(function (data) {
-      $('.js-events li').remove();
-      // 以下のコードを追加する
-      $(data).each(function(i,event) {
-        $('.js-events').append(
-          `<li class="event"><a href="/events/${event.id}">${event.title}</a></li>`
-        );
-      });
-    })
-  });
-  $('.js-text_field_place').on('keyup', function () {
-    var place = $.trim($(this).val());
-    $.ajax({
-      type: 'GET', // リクエストのタイプ
-      url: '/events/search', // リクエストを送信するURL
-      data:  { place: place}, // サーバーに送信するデータ
-      dataType: 'json' // サーバーから返却される型
-    }).done(function (data) {
-      $('.js-events li').remove();
-      // 以下のコードを追加する
-      $(data).each(function(i,event) {
-        $('.js-events').append(
-          `<li class="event"><a href="/events/${event.id}">${event.place}</a></li>`
-        );
-      });
-    })
-  });
-  $('.js-start_date_field').on('change', function () {
-    var start_date = $.trim($(this).val());
-    $.ajax({
-      type: 'GET', // リクエストのタイプ
-      url: '/events/search', // リクエストを送信するURL
-      data:  { start_date: start_date}, // サーバーに送信するデータ
-      dataType: 'json' // サーバーから返却される型
-    }).done(function (data) {
-      $('.js-events li').remove();
-      // 以下のコードを追加する
-      $(data).each(function(i,event) {
-        $('.js-events').append(
-          `<li class="event"><a href="/events/${event.id}">${event.start_date}</a></li>`
-        );
-      });
-    })
-  });
-  $('.js-end_date_field').on('change', function () {
-    var end_date = $.trim($(this).val());
-    $.ajax({
-      type: 'GET', // リクエストのタイプ
-      url: '/events/search', // リクエストを送信するURL
-      data:  { end_date: end_date}, // サーバーに送信するデータ
-      dataType: 'json' // サーバーから返却される型
-    }).done(function (data) {
-      $('.js-events li').remove();
-      // 以下のコードを追加する
-      $(data).each(function(i,event) {
-        $('.js-events').append(
-          `<li class="event"><a href="/events/${event.id}">${event.end_date}</a></li>`
-        );
-      });
-    })
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    $('.js-text_field_title').on('keyup', function () {
+      var title = $.trim($(this).val());
+      console.log(title);
+      $.ajax({
+        type: 'GET', // リクエストのタイプ
+        url: '/events/search', // リクエストを送信するURL
+        data:  { title: title}, // サーバーに送信するデータ
+        dataType: 'json' // サーバーから返却される型
+      }).done(function (data) {
+        $('.js-events li').remove();
+        // 以下のコードを追加する
+        $(data).each(function(i,event) {
+          $('.js-events').append(
+            `<li class="event"><a href="/events/${event.id}">${event.title}</a></li>`
+          );
+        });
+      })
+    });
+    $('.js-text_field_place').on('keyup', function () {
+      var place = $.trim($(this).val());
+      $.ajax({
+        type: 'GET', // リクエストのタイプ
+        url: '/events/search', // リクエストを送信するURL
+        data:  { place: place}, // サーバーに送信するデータ
+        dataType: 'json' // サーバーから返却される型
+      }).done(function (data) {
+        $('.js-events li').remove();
+        // 以下のコードを追加する
+        $(data).each(function(i,event) {
+          $('.js-events').append(
+            `<li class="event"><a href="/events/${event.id}">${event.place}</a></li>`
+          );
+        });
+      })
+    });
+    $('.js-start_date_field').on('change', function () {
+      var start_date = $.trim($(this).val());
+      $.ajax({
+        type: 'GET', // リクエストのタイプ
+        url: '/events/search', // リクエストを送信するURL
+        data:  { start_date: start_date}, // サーバーに送信するデータ
+        dataType: 'json' // サーバーから返却される型
+      }).done(function (data) {
+        $('.js-events li').remove();
+        // 以下のコードを追加する
+        $(data).each(function(i,event) {
+          $('.js-events').append(
+            `<li class="event"><a href="/events/${event.id}">${event.start_date}</a></li>`
+          );
+        });
+      })
+    });
+    $('.js-end_date_field').on('change', function () {
+      var end_date = $.trim($(this).val());
+      $.ajax({
+        type: 'GET', // リクエストのタイプ
+        url: '/events/search', // リクエストを送信するURL
+        data:  { end_date: end_date}, // サーバーに送信するデータ
+        dataType: 'json' // サーバーから返却される型
+      }).done(function (data) {
+        $('.js-events li').remove();
+        // 以下のコードを追加する
+        $(data).each(function(i,event) {
+          $('.js-events').append(
+            `<li class="event"><a href="/events/${event.id}">${event.end_date}</a></li>`
+          );
+        });
+      })
+    });
   });
 });
